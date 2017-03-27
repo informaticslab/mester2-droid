596bfa94f1122f4d64d738fe8485d94c7f24de0c
Jason James <jaso66@gmail.com>
2017-03-27 14:47:25 -0400

    Removed leak canary code

--- app/src/main/java/gov/cdc/mester2/MyApplication.java
+++ app/src/main/java/gov/cdc/mester2/MyApplication.java
@@ -11,12 +11,6 @@ public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        if (LeakCanary.isInAnalyzerProcess(this)) {
-            // This process is dedicated to LeakCanary for heap analysis.
-            // You should not init your app in this process.
-            return;
-        }
-        LeakCanary.install(this);
 
         Realm.init(this);
         RealmConfiguration config = new RealmConfiguration.Builder().build();
